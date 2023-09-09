59dfa1cd3ef6a75e3dbe4bda31db5bb62ce303d7
Leonardo Gomes Anholetto <leogomesanholetto@gmail.com>
2023-09-09 15:59:41 -0300

    validacoes feitas...

--- build/web/index_controller.js
+++ build/web/index_controller.js
@@ -1,5 +1,43 @@
+var formContato = document.getElementById("btnEnviar");
+var nomeInput = document.getElementById("nome");
+var artistaInput = document.getElementById("artista");
+
+function validarNome() {
+    let nameError = document.getElementById("nomeError");
+    let nameRegex = /^[a-zA-Z0-9_ ]+$/;
+
+    nameError.textContent = "";
+
+    if (!nameRegex.test(nomeInput.value)) {
+        nameError.textContent = "Nome não deve conter caracteres especiais!";
+        nomeInput.classList.add("error-border");
+        return false;
+    } else {
+        nomeInput.classList.remove("error-border");
+        return true;
+    }
+}
+
+
+function validarArtista() {
+    let artistError = document.getElementById("artistaError");
+    let artistaRegex = /^[a-zA-Z0-9_ ]+$/;
+
+    artistError.textContent = "";
+
+    if (!artistaRegex.test(artistaInput.value)) {
+        artistError.textContent = "Artista não deve conter caracteres especiais!";
+        artistaInput.classList.add("error-border");
+        return false;
+    } else {
+        artistaInput.classList.remove("error-border");
+        return true;
+    }
+}
+
+
+
 function upload_musica(){
-    
     event.preventDefault();
     
     let dados = new FormData(document.getElementById("uploadMp3"));
@@ -12,3 +50,25 @@ function upload_musica(){
 
     document.getElementById("uploadMp3").reset();
 }
+
+function validarFormulario(event) {
+    let isNomeValid = validarNome();
+    let isArtistaValid = validarArtista();
+  
+    if (!isNomeValid || !isArtistaValid) {
+        event.preventDefault();
+        btnEnviar.disabled = true;
+        btnEnviar.style.backgroundColor =  "#91c6ff";
+    } else {
+        btnEnviar.disabled = false;
+        btnEnviar.style.backgroundColor =  "#007BFF";
+    }
+}
+
+
+nomeInput.addEventListener('blur', validarFormulario,false);
+artistaInput.addEventListener('blur', validarFormulario,false);
+
+formContato.addEventListener("submit", validarFormulario,false);
+
+btnEnviar.disabled = true;
